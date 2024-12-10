d5626a92af4c77f1e057ec6bde8a3912101a4924
shriramrahul7 <shriramrahul.pingali7@gmail.com>
2024-12-10 09:44:39 +0530

    add navigation

--- app/src/main/java/com/example/kriya/MainActivity.kt
+++ app/src/main/java/com/example/kriya/MainActivity.kt
@@ -4,22 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,37 +29,100 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.kriya.Frequency.*
 import com.example.kriya.ui.theme.KriyaTheme
+import kotlinx.serialization.Serializable
 import java.time.LocalTime
+import java.util.UUID
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
-import kotlin.time.TimeSource
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             KriyaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HabitsList(
-                        modifier = Modifier.padding(innerPadding),
-                        habits = Habit.previewHabitList // TODO change this to real data
-                    )
+                val navController = rememberNavController()
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = { Text("Kriya") },
+                            actions = {
+                                Icon(Icons.TwoTone.Settings, contentDescription = "Settings", modifier = Modifier.clickable {
+                                    navController.navigate(Screen.Settings)
+                                })
+                            }
+                        )
+                    },
+                ) { innerPadding ->
+                    Box (Modifier.padding(innerPadding)) {
+                        val habitsList = Habit.previewHabitList
+
+                        NavHost(navController, startDestination = Screen.Home) {
+                            composable<Screen.Home> {
+                                HabitsList(habitsList) { habit ->
+                                    navController.navigate(
+                                        Screen.HabitDetail(habit.uuid.toString())
+                                    )
+                                }
+                            }
+
+                            composable<Screen.HabitDetail> {
+                                val route: Screen.HabitDetail = it.toRoute()
+                                val habit = habitsList.first { habit ->
+                                    habit.uuid.toString() == route.habitId
+                                }
+                                HabitDetail(habit)
+                            }
+
+                            dialog<Screen.Settings> {
+
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+sealed class Screen {
+    @Serializable
+    data object Home
+
+    @Serializable
+    data class HabitDetail(val habitId: String)
+
+    @Serializable
+    data object Settings
+}
+
+
 @Composable
-fun HabitsList(habits: List<Habit>, modifier: Modifier = Modifier) {
-    LazyColumn (modifier = modifier) {
-        itemsIndexed(habits, key = {_, it -> it.title}) { index, habit ->
-            HabitListItem(habit, {})
+fun HabitsList(
+    habits: List<Habit>,
+    modifier: Modifier = Modifier,
+    navigateToDetails: (Habit) -> Unit,
+) {
+    LazyColumn(modifier = modifier) {
+        itemsIndexed(habits, key = { _, it -> it.title }) { index, habit ->
+            HabitListItem(habit, navigateToDetails)
         }
     }
+}
+
+@Composable
+fun HabitDetail(habit: Habit) {
+    HabitListItem(habit) { }
 }
 
 @Composable
@@ -87,7 +152,12 @@ enum class Frequency(var times: Int = 1) {
     DAILY, WEEKLY, BI_WEEKLY, MONTHLY, YEARLY, UNDEFINED
 }
 
-data class Habit(var title: String, var duration: Duration = 15.minutes, var startTime: LocalTime? = null, var frequency: Frequency = UNDEFINED) {
+data class Habit(
+    var title: String, var duration: Duration = 15.minutes,
+    var startTime: LocalTime? = null,
+    var frequency: Frequency = UNDEFINED,
+    val uuid: UUID = UUID.randomUUID(),
+) {
     companion object {
         val previewHabitList = listOf(
             Habit("Water plants", frequency = DAILY, duration = 5.minutes),
@@ -105,9 +175,9 @@ fun HomePreview() {
     KriyaTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             HabitsList(
+                habits = Habit.previewHabitList,
                 modifier = Modifier.padding(innerPadding),
-                habits = Habit.previewHabitList // TODO change this to real data
-            )
+            ) {}
         }
     }
 }
